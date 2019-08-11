3cb33eb7a7ec2c433a4af01f32f35c72f9c28f5f
flux <25628292+fluxionary@users.noreply.github.com>
2019-08-11 01:47:19 +0000

    convert asn names to utf8

--- update_tables.sh
+++ update_tables.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-wget -O data-raw-table    http://thyme.apnic.net/current/data-raw-table
-wget -O data-used-autnums http://thyme.apnic.net/current/data-used-autnums
+wget -O data-raw-table http://thyme.apnic.net/current/data-raw-table
+wget -O -              http://thyme.apnic.net/current/data-used-autnums | iconv -t utf-8 -f iso-8859-1 > data-used-autnums
